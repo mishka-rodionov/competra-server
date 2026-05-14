11cb39d292c69143e9b0a54b05b8f1aebe85237f
mishka_rodionov <mishka_rodionov@mail.ru>
2026-05-14 15:32:39 -0700

    master: fix imports

--- eSport/src/main/kotlin/Frameworks.kt
+++ eSport/src/main/kotlin/Frameworks.kt
@@ -1,6 +1,5 @@
 package com.sportenth
 
-import com.codahale.metrics.*
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.RabbitMQ
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.*
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.rabbitMQ
@@ -8,7 +7,6 @@ import io.ktor.http.*
 import io.ktor.serialization.gson.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.metrics.dropwizard.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.contentnegotiation.*
