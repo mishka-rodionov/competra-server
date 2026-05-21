d7b0a8c14ea4ecab3cd9dc2e62bd194c199a9b2e
mishka_rodionov <mishka_rodionov@mail.ru>
2026-05-21 08:21:26 -0700

    master: change access token life time to 1 hour

--- eSport/src/main/kotlin/data/services/smtp/tokens/JWT.kt
+++ eSport/src/main/kotlin/data/services/smtp/tokens/JWT.kt
@@ -4,7 +4,7 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import java.util.Date
 import java.util.UUID
-import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.hours
 
 val jwtIssuer: String = System.getenv("JWT_ISSUER") ?: "ktor_server"
 val jwtAudience: String = System.getenv("JWT_AUDIENCE") ?: "ktor_audience"
@@ -15,7 +15,7 @@ fun generateAccessToken(userId: String): String =
         .withIssuer(jwtIssuer)
         .withAudience(jwtAudience)
         .withClaim("userId", userId)
-        .withExpiresAt(Date(System.currentTimeMillis() + 15.minutes.inWholeMilliseconds))
+        .withExpiresAt(Date(System.currentTimeMillis() + 1.hours.inWholeMilliseconds))
         .sign(Algorithm.HMAC256(jwtSecret))
 
 fun generateRefreshToken(): String = UUID.randomUUID().toString()
